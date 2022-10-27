25d013723e7557ca23cb662415490e2125d6787c
Erodraeda <lucaswalterm@gmail.com>
2022-10-27 12:36:15 -0300

    commented database helper for now since it's not working

--- lib/util/databaseHelper.dart
+++ lib/util/databaseHelper.dart
@@ -1,73 +1,70 @@
-import 'dart:io';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
+// import 'dart:io';
 
-class DatabaseHelper {
-  static const _databaseName = "ExemploDB.db";
-  static const _databaseVersion = 1;
-  static const table = 'TBPessoa';
+// class DatabaseHelper {
+//   static const _databaseName = "ExemploDB.db";
+//   static const _databaseVersion = 1;
+//   static const table = 'TBPessoa';
 
-  DatabaseHelper._privateConstructor();
+//   DatabaseHelper._privateConstructor();
 
-  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+//   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
-  static Database? _database;
+//   static Database? _database;
 
-  Future<Database> get database async {
-    if (_database != null) return _database!;
+//   Future<Database> get database async {
+//     if (_database != null) return _database!;
 
-    _database = await _initDatabase();
-    return _database!;
-  }
+//     _database = await _initDatabase();
+//     return _database!;
+//   }
 
-  _initDatabase() async {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+//   _initDatabase() async {
+//     Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
-    String path = join(documentsDirectory.path, _databaseName);
+//     String path = join(documentsDirectory.path, _databaseName);
 
-    return await openDatabase(path,
-        version: _databaseVersion, onCreate: _onCreate);
-  }
+//     return await openDatabase(path,
+//         version: _databaseVersion, onCreate: _onCreate);
+//   }
 
-  Future _onCreate(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE $table (
+//   Future _onCreate(Database db, int version) async {
+//     await db.execute('''
+//       CREATE TABLE $table (
 
-        id INTEGER PRIMARY KEY,
-        nome TEXT NOT NULL,
-        email TEXT NOT NULL,
-        aniversario TEXT NOT NULL,
-        categoria TEXT NOT NULL
+//         id INTEGER PRIMARY KEY,
+//         nome TEXT NOT NULL,
+//         email TEXT NOT NULL,
+//         aniversario TEXT NOT NULL,
+//         categoria TEXT NOT NULL
 
-      )
-    ''');
-  }
+//       )
+//     ''');
+//   }
 
-  Future<int> insert(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    return await db.insert(table, row);
-  }
+//   Future<int> insert(Map<String, dynamic> row) async {
+//     Database db = await instance.database;
+//     return await db.insert(table, row);
+//   }
 
-  Future<List<Map<String, dynamic>>> queryAllRows() async {
-    Database db = await instance.database;
-    return await db.query(table);
-  }
+//   Future<List<Map<String, dynamic>>> queryAllRows() async {
+//     Database db = await instance.database;
+//     return await db.query(table);
+//   }
 
-  Future<int?> queryRowCount() async {
-    Database db = await instance.database;
-    return Sqflite.firstIntValue(
-        await db.rawQuery('SELECT COUNT(*) FROM $table'));
-  }
+//   Future<int?> queryRowCount() async {
+//     Database db = await instance.database;
+//     return Sqflite.firstIntValue(
+//         await db.rawQuery('SELECT COUNT(*) FROM $table'));
+//   }
 
-  Future<int> update(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    int id = row['id'];
-    return await db.update(table, row, where: 'id = ?', whereArgs: [id]);
-  }
+//   Future<int> update(Map<String, dynamic> row) async {
+//     Database db = await instance.database;
+//     int id = row['id'];
+//     return await db.update(table, row, where: 'id = ?', whereArgs: [id]);
+//   }
 
-  Future<int> delete(int id) async {
-    Database db = await instance.database;
-    return await db.delete(table, where: 'id = ?', whereArgs: [id]);
-  }
-}
+//   Future<int> delete(int id) async {
+//     Database db = await instance.database;
+//     return await db.delete(table, where: 'id = ?', whereArgs: [id]);
+//   }
+// }
