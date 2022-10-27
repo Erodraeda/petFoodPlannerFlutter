f687a2b733c29383f487ce6d2f7f8b35700a66f3
Erodraeda <lucaswalterm@gmail.com>
2022-10-27 16:17:15 -0300

    fixed identation and added route to buttons

--- lib/views/my_homepage.dart
+++ lib/views/my_homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:PetFoodPlanner/views/myapp.dart';
+import 'package:PetFoodPlanner/views/cadastro_pet.dart';
 
 class MyHomePage extends StatefulWidget {
   static const nomeRota = '/myhomepage';
@@ -22,21 +23,22 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             children: [
               Expanded(
-                child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                    padding: const EdgeInsets.only(left: 64.0, right: 64.0, top: 24.0, bottom: 8.0),
+                  child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        left: 64.0, right: 64.0, top: 24.0, bottom: 8.0),
                     child: Container(
                       height: 120,
                       width: 50,
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.deepPurple, width: 3.0)
-                      ),
+                          border:
+                              Border.all(color: Colors.deepPurple, width: 3.0)),
                       child: const Align(
                         alignment: Alignment.centerLeft,
                         child: Padding(
-                          padding: EdgeInsets.only(left:24.0),
+                          padding: EdgeInsets.only(left: 24.0),
                           child: Text(
                             'Nome do Gato',
                             maxLines: 1,
@@ -55,9 +57,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       // ) // TODO: Adicionar checkboxes no lado direito do container
                     ),
                   );
-                  },
-                )
-              )
+                },
+              ))
             ],
           ),
         ));
@@ -79,17 +80,17 @@ class _MyHomePageState extends State<MyHomePage> {
           ListTile(
               title: const Text('Lista de seus pets!'),
               onTap: () {
-                // Navigator.pushNamed(context, CadastrarPessoa.nomeRota);
+                Navigator.pushNamed(context, MyHomePage.nomeRota);
               }),
           ListTile(
               title: Text('Detalhes dos seus pets!'),
               onTap: () {
-                // Navigator.pushNamed(context, CadastrarPessoa.nomeRota);
+                // Navigator.pushNamed(context, CadastrarPet.nomeRota);
               }),
           ListTile(
               title: Text('Cadastre mais pets!'),
               onTap: () {
-                // Navigator.pushNamed(context, CadastrarPessoa.nomeRota);
+                Navigator.pushNamed(context, CadastrarPet.nomeRota);
               }),
           ListTile(
               title: Text('Configure os horários de alimentação!'),
