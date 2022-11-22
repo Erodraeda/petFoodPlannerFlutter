a339b235aa58fdf95314b805d60a2800da6e298f
Erodraeda <lucaswalterm@gmail.com>
2022-11-22 14:18:07 -0300

    Fixed and finished model for feeding

--- lib/models/alimentacao.dart
+++ lib/models/alimentacao.dart
@@ -1,18 +1,23 @@
+// ignore_for_file: non_constant_identifier_names
+
 class Alimentacao {
   int? id;
-  int? petID;
-  DateTime? dataAlimentacao;
+  int? pet_id;
+  String? data_alimentacao;
   int? alimentacao1;
   int? alimentacao2;
   int? alimentacao3;
 
-  Alimentacao(int petID, DateTime dataAlimentacao, int alimentacao1,
-      int alimentacao2, int alimentacao3);
+  Alimentacao.withA(this.pet_id, this.data_alimentacao, this.alimentacao1,
+      this.alimentacao2, this.alimentacao3,
+      [this.id]);
+
+  Alimentacao();
 
   Alimentacao.fromMap(Map map) {
     id = map['id'];
-    petID = map['petID'];
-    dataAlimentacao = map['dataAlimentacao'];
+    pet_id = map['pet_id'];
+    data_alimentacao = map['data_alimentacao'];
     alimentacao1 = map['alimentacao1'];
     alimentacao2 = map['alimentacao2'];
     alimentacao3 = map['alimentacao3'];
@@ -21,8 +26,8 @@ class Alimentacao {
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
       'id': id,
-      'petID': petID,
-      'dataAlimentacao': dataAlimentacao,
+      'pet_id': pet_id,
+      'data_alimentacao': data_alimentacao,
       'alimentacao1': alimentacao1,
       'alimentacao2': alimentacao2,
       'alimentacao3': alimentacao3,
@@ -35,6 +40,6 @@ class Alimentacao {
 
   @override
   String toString() {
-    return ("Id: $id, Pet ID: $petID, Data da Alimentação: $dataAlimentacao, Alimentação 1: $alimentacao1, Alimentação 2: $alimentacao2, Alimentação 3: $alimentacao3");
+    return ("Id: $id, Pet ID: $pet_id, Data da Alimentação: $data_alimentacao, Alimentação 1: $alimentacao1, Alimentação 2: $alimentacao2, Alimentação 3: $alimentacao3");
   }
 }
